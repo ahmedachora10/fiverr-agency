462d5e66a57ac0adf74901d913880c487eeced70
ahmedachora10 <gjr8195@gmail.com>
2025-08-26 18:37:30 +0100

    -

--- resources/js/components/Admin/PostForm.tsx
+++ resources/js/components/Admin/PostForm.tsx
@@ -366,6 +366,7 @@ export default function PostForm({
                       accept="image/*"
                       className="mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
                       onChange={(e) => setData('featured_image', e.target.files?.[0])}
+                      onInvalid={(e) => e.preventDefault()}
                     />
                     <InputError message={errors.featured_image} className="mt-2" />
                   </div>
