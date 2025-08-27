56b5c45a60404efff5e800f2e0f4d5eeb03e5a17
ahmedachora10 <gjr8195@gmail.com>
2025-08-27 16:25:16 +0100

    improve all blogs page filter and design

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Api\Admin\UploadController;
 
 Route::get('/', function () {
     return Inertia::render('Index', [
-        'posts' => Post::with(['category', 'tags', 'author'])->take(6)->get(),
+        'posts' => Post::with(['category', 'tags', 'author'])->published()->take(6)->get(),
     ]);
 })->name('home');
 
