c2afbea66d66749855a8e41f06b7c90502bd8dd5
ahmedachora10 <gjr8195@gmail.com>
2025-09-03 13:32:50 +0100

    change title name

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -46,7 +46,7 @@ class HandleInertiaRequests extends Middleware
 
         return [
             ...parent::share($request),
-            'name' => config('app.name'),
+            'name' => setting('app_name') ?? config('app.name'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
